67f6a10d1e0a8a91c713ce2751a857c98c633fc1
Andrew Min <minandrew0711@gmail.com>
2022-09-16 16:58:33 -0700

    changed everything

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 var currentDate = moment();
-$("#currentDay").text(currentDate.format("dddd MMM Do, YYYY"))
+var currentHour = moment().format("h");
+$("#currentDay").text(currentDate.format("dddd MMM Do, YYYY"));
 
 var container = $(".container")
-var hours = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
+var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
 var period = ["AM", "PM"]
 
 for (var i = 0; i < 9; i++) {
@@ -11,7 +12,8 @@ for (var i = 0; i < 9; i++) {
     var hourSection = document.createElement('p');
     hourSection.setAttribute("class", "hour col-2");
     var timeBlocks = document.createElement('textarea');
-    timeBlocks.setAttribute("class", "textarea col-8");
+    timeBlocks.setAttribute("class", "textarea col-8 description");
+    timeBlocks.setAttribute("id", "textarea" + (i + 1));
     var saveBtn = document.createElement('button');
     saveBtn.setAttribute("class", "saveBtn col-2");
     saveBtn.setAttribute("id", "save" + (i + 1));
@@ -26,18 +28,73 @@ for (var i = 0; i < 9; i++) {
         hourSection.textContent = hours[i] + period[1];
     };
 
-    // function save() {
-    //     var data = timeBlocks.value;
-    //     console.log(data);
-    // }
-
-    // var saveBtnEvent_i = $("#save" + (i + 1));
-    // saveBtnEvent_i.addEventListener("click", save);
+    if (currentHour === hours[i]) {
+        timeBlocks.setAttribute("class", "textarea col-8 description present")
+    } else if (hours.slice(i) == hours[i]) {
+        timeBlocks.setAttribute("class", "textarea col-8 description future")
+    } else {
+        timeBlocks.setAttribute("class", "textarea col-8 description past")
+    }
 };
 
-// function save() {
-//     localStorage.setItem('todo', JSON.stingify(timeBlocks.value));
-//     var todo = JSON.parse(localStorage.getItem('todo'));
-//     timeBlocks.textContent = todo;
-// };
+// variables for save buttons 
+var saveBtn1 = document.querySelector("#save1");
+var saveBtn2 = document.querySelector("#save2");
+var saveBtn3 = document.querySelector("#save3");
+var saveBtn4 = document.querySelector("#save4");
+var saveBtn5 = document.querySelector("#save5");
+var saveBtn6 = document.querySelector("#save6");
+var saveBtn7 = document.querySelector("#save7");
+var saveBtn8 = document.querySelector("#save8");
+var saveBtn9 = document.querySelector("#save9");
 
+function save() {
+    // variables for textareas
+    var textarea1 = document.querySelector("#textarea1");
+    var textarea2 = document.querySelector("#textarea2");
+    var textarea3 = document.querySelector("#textarea3");
+    var textarea4 = document.querySelector("#textarea4");
+    var textarea5 = document.querySelector("#textarea5");
+    var textarea6 = document.querySelector("#textarea6");
+    var textarea7 = document.querySelector("#textarea7");
+    var textarea8 = document.querySelector("#textarea8");
+    var textarea9 = document.querySelector("#textarea9");
+
+    // saves the text to localstorage for each textarea
+    localStorage.setItem("save1", textarea1.value);
+    localStorage.setItem("save2", textarea2.value);
+    localStorage.setItem("save3", textarea3.value);
+    localStorage.setItem("save4", textarea4.value);
+    localStorage.setItem("save5", textarea5.value);
+    localStorage.setItem("save6", textarea6.value);
+    localStorage.setItem("save7", textarea7.value);
+    localStorage.setItem("save8", textarea8.value);
+    localStorage.setItem("save9", textarea9.value);
+};
+
+// eventlistener for save buttons
+saveBtn1.addEventListener("click", save);
+saveBtn2.addEventListener("click", save);
+saveBtn3.addEventListener("click", save);
+saveBtn4.addEventListener("click", save);
+saveBtn5.addEventListener("click", save);
+saveBtn6.addEventListener("click", save);
+saveBtn7.addEventListener("click", save);
+saveBtn8.addEventListener("click", save);
+saveBtn9.addEventListener("click", save);
+
+// saves the text in the todo when refreshed
+textarea1.textContent = localStorage.getItem("save1");
+textarea2.textContent = localStorage.getItem("save2");
+textarea3.textContent = localStorage.getItem("save3");
+textarea4.textContent = localStorage.getItem("save4");
+textarea5.textContent = localStorage.getItem("save5");
+textarea6.textContent = localStorage.getItem("save6");
+textarea7.textContent = localStorage.getItem("save7");
+textarea8.textContent = localStorage.getItem("save8");
+textarea9.textContent = localStorage.getItem("save9");
+
+// clears the localstorage every day 
+if (moment().format("hA") === "12AM") {
+    localStorage.clear();
+};
